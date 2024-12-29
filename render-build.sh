fd9cfdcc7d3e8e177c5260c7c2694e1d03c5b304
Ajit kulkarni <160049856+Ajitkulkarni1@users.noreply.github.com>
2024-12-29 23:31:46 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-# Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oE "[0-9]+.[0-9]+.[0-9]+")
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
